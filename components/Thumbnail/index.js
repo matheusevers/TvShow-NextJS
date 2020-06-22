320c1dd3df0f99712f8caa9a0ad2b29d4808bd20
Matheus <matheus.evers@ab-inbev.com>
2020-06-22 08:29:17 -0300

    Adding styles to the components

---
+++ components/Thumbnail/index.js
@@ -0,0 +1,14 @@
+import ThumbnailStyles from "./styles";
+
+const Thumbnail = ({ imageUrl, caption }) => {
+  return (
+    <div className="thumbnail">
+      <img src={imageUrl} className="thumbnail__image" />
+      <h4 className="thumbnail__caption">{caption}</h4>
+
+      <style jsx>{ThumbnailStyles}</style>
+    </div>
+  );
+};
+
+export default Thumbnail;
--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,10 +1,23 @@
 import { useEffect } from "react";
 import axios from "axios";
+import Thumbnail from "../../components/Thumbnail";
 
-const Home = (props) => {
-  console.log(props);
+const Home = ({ shows }) => {
+  const renderShows = () => {
+    return shows.map((showItem, index) => {
+      const { show } = showItem;
 
-  return <h1>test</h1>;
+      return (
+        <li key={index}>
+          <Thumbnail
+            imageUrl={show.image ? show.image.medium : ""}
+            caption={show.name}
+          />
+        </li>
+      );
+    });
+  };
+  return <ul className="tvshows">{renderShows()}</ul>;
 };
 
 // Home.getInitialProps = async () => {
@@ -16,9 +29,10 @@ const Home = (props) => {
 //   };
 // };
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+  const country = context.params.country || "us";
   const response = await axios.get(
-    "http://api.tvmaze.com/schedule?country=US&date=2014-12-01"
+    `http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
   );
   return {
     props: { shows: response.data },
